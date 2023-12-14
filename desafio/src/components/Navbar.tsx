38203ea436a8ddddfb49b9817c77a8578ffcb5a7
Fernando pereira <fernandomendespereira@hotmail.com>
2023-12-14 11:17:12 -0300

    fix bugs

--- desafio/src/components/Navbar.tsx
+++ desafio/src/components/Navbar.tsx
@@ -15,12 +15,12 @@ const Navbar: React.FC = () => {
       <button onClick={toggleSidebar}>
         <Image src="/menu.svg" alt="Menu" height={20} width={20} />
       </button>
-      
+
       <Image src="/logo.svg" alt="" height={32} width={32} />
       <div className="flex items-center gap-4 max-md:hidden">
         <Image src="/notifications.svg" alt="notifications" height={16} width={16} />
         <Image src="/plus.svg" alt="plus" height={21} width={27} />
-        <Image src="/user.svg" alt="notifications" height={32} width={32} />
+        
       </div>
 
       {isSidebarOpen && <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />}
