d7f4abeecf1076b4acd3db14bcef0662e7235c9f
Fernando pereira <fernandomendespereira@hotmail.com>
2023-12-14 08:24:22 -0300

    fix bugs

--- desafio/src/components/Navbar.tsx
+++ desafio/src/components/Navbar.tsx
@@ -3,11 +3,6 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Sidebar from "./Sidebar";
 
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
- 
-import { Button } from "@/components/ui/button"
-
 const Navbar: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -16,20 +11,18 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-between px-16 py-2 bg-gray-800 text-white">
+    <div className="flex items-center justify-between w-full px-16 py-2 bg-gray-800 text-white">
       <button onClick={toggleSidebar}>
         <Image src="/menu.svg" alt="Menu" height={20} width={20} />
       </button>
-
+      
       <Image src="/logo.svg" alt="" height={32} width={32} />
-
       <div className="flex items-center gap-4 max-md:hidden">
         <Image src="/notifications.svg" alt="notifications" height={16} width={16} />
         <Image src="/plus.svg" alt="plus" height={21} width={27} />
         <Image src="/user.svg" alt="notifications" height={32} width={32} />
       </div>
 
-      {/* Adicione a barra lateral */}
       {isSidebarOpen && <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />}
     </div>
   );
